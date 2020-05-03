8641df8a3d25e89450d6a234a4640e2826ab5744
noxiss <kamiljakubczak@outlook.com>
2020-05-03 17:53:05 +0200

    views cleaning

--- mainapp/views.py
+++ mainapp/views.py
@@ -40,20 +40,17 @@ mapping = {
 def load_types(request):
     types_id = request.GET.get('type')
     categories = Category.objects.filter(parent_type_name_id=types_id)
-    print(types_id,'load_types')
     return render(request, 'mainapp/type_dropdown_list_options.html', {'categories': categories})
 
 def load_categories(request):
     category_id = request.GET.get('category')
     subcategories = SubCategory.objects.filter(parent_category_name_id=category_id)#category_id)#.order_by('citi_name')
-    print(category_id)
     return render(request, 'mainapp/category_dropdown_list_options.html', {'subcategories': subcategories})
 
 def load_payments(request):
     print('vies.payments')
     types_id = request.GET.get('payment')
     payments = Payment.objects.filter(parent_type_name_id=types_id)
-    print(types_id, 'load_payments')
     return render(request, 'mainapp/payment_dropdown_list_options.html', {'payments': payments})
 
 #def month_view()
@@ -66,10 +63,6 @@ def view_month(request,year,month):
     yearly_sum = Entry.objects.filter(transaction_date__year__lte=year)
     to_month_sum = Entry.objects.filter(transaction_date__lte=next_month)
 
-    # to_month_sum = Entry.objects.filter(transaction_date__year__lte=year,
-    #                                 transaction_date__month__lte=month)
-    print(80*'#')
-    print(to_month_sum.order_by('-transaction_date'))
     monthly_sum = yearly_sum.filter(transaction_date__month=month)
     temp_month = month
     years = Entry.objects.values_list('transaction_date__year').distinct()
@@ -89,8 +82,6 @@ def view_month(request,year,month):
    
     details = monthly_sum.order_by('-transaction_date')
 
-    # account_sum = monthly_sum.filter(payment=mapping['account_inc']).aggregate(Sum('amount'))
-    # account_sum = account_sum['amount__sum']
     account_sum = payment_result(to_month_sum,'account_inc','account_exp','amount')
     account_sum += payment_result(to_month_sum,'retToAcc','accToRet','amount')
     account_sum += payment_result(to_month_sum,'emrToAcc','accToEmr','amount')
@@ -107,11 +98,8 @@ def view_month(request,year,month):
     entertainment_sum = payment_result(to_month_sum,'accToEnt','entToAcc','amount')
     savings_sum = retirement_sum+emergency_sum+target_sum+entertainment_sum
 
-    # cash_sum =  monthly_sum.filter(payment=mapping['cash_inc']).aggregate(Sum('amount'))
-    # cash_sum = cash_sum['amount__sum']
     
     cash_sum = payment_result(to_month_sum,'cash_inc','cash_exp','amount')
-    print(cash_sum,"cash_sum")
     cash_sum += payment_result(to_month_sum,'accToCsh','cshToAcc','amount')
 
     income_sum = getTypeSum(monthly_sum,mapping['income'])
@@ -127,16 +115,12 @@ def view_month(request,year,month):
     chart_labels.append('expenditures')
     chart_percentage.append(income_sum/(income_sum+expenditures_sum))
     chart_percentage.append(expenditures_sum/(income_sum+expenditures_sum))
-    # for i in chart_values:
-    #     print(i)
     categories_amount = get_categories_amount(details,categs)
-    # print('beore' + categories_amount)
     json_values = json.dumps(chart_values, cls=DjangoJSONEncoder)
     json_percentage = json.dumps(chart_percentage,cls=DjangoJSONEncoder)
     json_labels = json.dumps(chart_labels)
     json_categories = json.dumps(categs)
     json_categories_amount = json.dumps(categories_amount,cls=DjangoJSONEncoder)
-    # print( 'json!!' + json_categories_amount)
     contex = {'account_sum': account_sum,
             'cash_sum': cash_sum,
             'income_sum': income_sum,
@@ -163,15 +147,12 @@ def view_month(request,year,month):
 def get_categories_list(monthly_sum):
     category_list = monthly_sum.filter(types =mapping['expenditures']).values_list('category')
     
-    #print('1' + category_list)
     temp_list = []
     for i in category_list:
         temp_list.append(i[0])
     category_list = set(category_list)
-    # print('2' + str(category_list))
     ready_list =[]
     for cat in category_list:
-        #print(((Category.objects.filter(id=cat[0]).values_list('category_name'))[0])[0])
         ready_list.append(((Category.objects.filter(id=cat[0]).values_list('category_name'))[0])[0])
     print('function list' + str(ready_list))
     return ready_list
@@ -180,7 +161,6 @@ def get_categories_amount(monthly_sum, category_list):
     amount_list =[]
     for category in category_list:
         temp_amount = monthly_sum.filter(category__category_name=category).aggregate(Sum('amount'))
-        #print(temp_amount)
         amount_list.append(temp_amount['amount__sum'])
 
     return amount_list
@@ -207,8 +187,6 @@ def full_list(request):
     details = get_all.order_by('-transaction_date')
     monthly_sum = Entry.objects.all()
 
-    # account_sum = monthly_sum.filter(payment=mapping['account_inc']).aggregate(Sum('amount'))
-    # account_sum = account_sum['amount__sum']
     account_sum = payment_result(monthly_sum,'account_inc','account_exp','amount')
     account_sum += payment_result(monthly_sum,'retToAcc','accToRet','amount')
     account_sum += payment_result(monthly_sum,'emrToAcc','accToEmr','amount')
@@ -221,9 +199,6 @@ def full_list(request):
     entertainment_sum = payment_result(monthly_sum,'accToEnt','entToAcc','amount')
     savings_sum = retirement_sum+emergency_sum+target_sum+entertainment_sum
 
-
-    # cash_sum =  monthly_sum.filter(payment=mapping['cash_inc']).aggregate(Sum('amount'))
-    # cash_sum = cash_sum['amount__sum']
     cash_sum = payment_result(monthly_sum,'cash_inc','cash_exp','amount')
     income_sum = getTypeSum(monthly_sum,mapping['income'])
     expenditures_sum = getTypeSum(monthly_sum,mapping['expenditures'])
@@ -232,11 +207,6 @@ def full_list(request):
     if expenditures_sum == None:
         expenditures_sum = 0
     result_sum = income_sum - expenditures_sum
-
-    #json_ret = json.dumps(ret_sum, cls=DjangoJSONEncoder)
-
-    for i in details:
-        print(i.id)
 
     contex = {'account_sum': account_sum,
             'cash_sum': cash_sum,
@@ -270,8 +240,6 @@ def getTypeSum(dataset,transaction_type):
 
 
 def payment_result(data,pos_amount,neg_amount,col):
-    #print(mapping[pos_amount])
-    #cash_sum =  data.filter(payment=mapping['cash_inc']).aggregate(Sum('amount'))
     try:
         pos_sum = data.filter(payment=mapping[pos_amount]).aggregate(Sum(col))
         pos_sum = pos_sum[col+'__sum']
@@ -286,7 +254,6 @@ def payment_result(data,pos_amount,neg_amount,col):
         pos_sum =0
     if neg_sum == None:
         neg_sum =0
-    print(pos_sum,neg_sum)
     result = float(pos_sum) - float(neg_sum)
     return round(result,2)
 
@@ -305,7 +272,6 @@ def savings_graph(request):
 
     for year in years:
         months = Entry.objects.values_list('transaction_date__month').order_by('transaction_date__month').distinct()
-        print(months)
         for month in months:
             year_month.append(str(year[0]) + '-' + str(month[0]))
             year_month_data = Entry.objects.filter(transaction_date__year=year[0]).filter(transaction_date__month=month[0])
@@ -343,7 +309,4 @@ def del_entry(request,entry_id):
 def show_transactions(request):
     transactions = read_csv.TransactionList('/home/noxiss/Downloads/lista_operacji_200201_200229_202003071553459319.csv')
     transactions_list = transactions.transactions
-    # for transaction in transactions_list:
-    #     print(transaction.account)
-    
     return render(request,'mainapp/transaction_view.html',{'transactions': transactions_list})
